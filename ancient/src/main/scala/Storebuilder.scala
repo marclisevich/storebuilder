7453e1ecdd825b8115a71bd55568d5e542d8eee5
Marc Lisevich <cybronerd@gmail.ca>
2017-03-02 17:23:02 -0700

    Added maven project good to go

---
+++ ancient/src/main/scala/Storebuilder.scala
@@ -0,0 +1,16 @@
+class StoreBuilder {
+
+  object Intro {
+    def apply() : String {
+      "Welcome to the newly configuring Nerd Consulting Store"
+    }
+
+    class Intro( ) {
+
+    }
+
+  }
+  case class ByFours(field1: String, field2: String field3: String, field4 : String) {
+  }
+
+}
